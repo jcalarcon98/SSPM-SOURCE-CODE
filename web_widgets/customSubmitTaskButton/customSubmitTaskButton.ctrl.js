487ffd97a664f13291fb65e5290b4076d1e0ed63
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2021-02-13 14:44:23 -0500

    [IMP]: Improve Provide Information Needed for the process.
    SSPM-86: DONE.
    SSPM-87: DONE, custom widget added that works at the moment 'Submit Task' in all tasks

---
+++ web_widgets/customSubmitTaskButton/customSubmitTaskButton.ctrl.js
@@ -0,0 +1,117 @@
+function PbButtonCtrl($scope, $http, $location, $log, $window, localStorageService, modalService) {
+
+  'use strict';
+
+  var vm = this;
+
+  this.action = function action() {
+      
+      Swal.fire({
+          title: $scope.properties.titleQuestion,
+          text: $scope.properties.textQuestion,
+          type: 'question',
+          showCancelButton: true,
+          customClass: {
+            confirmButton: 'btn btn-success mr-5',
+            cancelButton: 'btn btn-danger'
+          },
+          buttonsStyling: false,   
+          confirmButtonText: $scope.properties.confirmText,
+          cancelButtonText: $scope.properties.cancelText,
+          footer: '<p class="text-primary"><b>Seguimiento al Sílabo y Plan de mejoras </b></p>',
+        }).then((result) => {
+          if (result.value) {
+             submitTask();
+          }
+        })
+  };
+  /**
+   * Execute a get/post request to an URL
+   * It also bind custom data from success|error to a data
+   * @return {void}
+   */
+  function doRequest(method, url, params) {
+    vm.busy = true;
+    var req = {
+      method: method,
+      url: url,
+      data: angular.copy($scope.properties.dataToSend),
+      params: params
+    };
+
+    return $http(req)
+      .success(function(data, status) {
+        $scope.properties.dataFromSuccess = data;
+        $scope.properties.responseStatusCode = status;
+        $scope.properties.dataFromError = undefined;
+        notifyParentFrame({ message: 'success', status: status, dataFromSuccess: data, dataFromError: undefined, responseStatusCode: status});
+        if ($scope.properties.targetUrlOnSuccess && method !== 'GET') {
+          redirectIfNeeded();
+        }
+        closeModal($scope.properties.closeOnSuccess);
+      })
+      .error(function(data, status) {
+        $scope.properties.dataFromError = data;
+        $scope.properties.responseStatusCode = status;
+        $scope.properties.dataFromSuccess = undefined;
+        notifyParentFrame({ message: 'error', status: status, dataFromError: data, dataFromSuccess: undefined, responseStatusCode: status});
+      })
+      .finally(function() {
+        vm.busy = false;
+      });
+  }
+
+  function redirectIfNeeded() {
+    var iframeId = $window.frameElement ? $window.frameElement.id : null;
+    //Redirect only if we are not in the portal or a living app
+    if (!iframeId || iframeId && iframeId.indexOf('bonitaframe') !== 0) {
+      $window.location.assign($scope.properties.targetUrlOnSuccess);
+    }
+  }
+
+  function notifyParentFrame(additionalProperties) {
+    if ($window.parent !== $window.self) {
+      var dataToSend = angular.extend({}, $scope.properties, additionalProperties);
+      $window.parent.postMessage(JSON.stringify(dataToSend), '*');
+    }
+  }
+
+  function getUserParam() {
+    var userId = getUrlParam('user');
+    if (userId) {
+      return { 'user': userId };
+    }
+    return {};
+  }
+
+  /**
+   * Extract the param value from a URL query
+   * e.g. if param = "id", it extracts the id value in the following cases:
+   *  1. http://localhost/bonita/portal/resource/process/ProcName/1.0/content/?id=8880000
+   *  2. http://localhost/bonita/portal/resource/process/ProcName/1.0/content/?param=value&id=8880000&locale=en
+   *  3. http://localhost/bonita/portal/resource/process/ProcName/1.0/content/?param=value&id=8880000&locale=en#hash=value
+   * @returns {id}
+   */
+  function getUrlParam(param) {
+    var paramValue = $location.absUrl().match('[//?&]' + param + '=([^&#]*)($|[&#])');
+    if (paramValue) {
+      return paramValue[1];
+    }
+    return '';
+  }
+
+  function submitTask() {
+    var id;
+    id = getUrlParam('id');
+    if (id) {
+      var params = getUserParam();
+	    params.assign = $scope.properties.assign;
+      doRequest('POST', '../API/bpm/userTask/' + getUrlParam('id') + '/execution', params).then(function() {
+        localStorageService.delete($window.location.href);
+      });
+    } else {
+      $log.log('Impossible to retrieve the task id value from the URL');
+    }
+  }
+
+}
